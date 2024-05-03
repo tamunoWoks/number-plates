68a31a5ec5ba77c5bbcaacb81bb57093572fbfc8
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-03 22:57:25 +0100

    Include main()

---
+++ plates.py
@@ -0,0 +1,12 @@
+def main():
+    plate = input("Plate: ").upper()
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid():
+
+
+if __name__ == "__main__":
+    main()
