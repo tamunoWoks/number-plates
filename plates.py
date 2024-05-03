7972ed6e6205cba66a30dddcb187265dd005d33a
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-03 23:08:08 +0100

    Updated code

--- plates.py
+++ plates.py
@@ -5,7 +5,25 @@ def main():
     else:
         print("Invalid")
 
-def is_valid():
+def is_valid(text):
+    #check if plate is within 2-6 characters
+    if len(text) >= 2 and len(text) <=6:
+        #make sure it can accept only alphabets
+        if text.isalpha():
+            return True
+        #check if first two characters are alphabets and if input is alphanumeric
+        elif text.isalnum() and text[0:2].isalpha():
+            for char in text:
+                #check for first digit
+                if char.isdigit():
+                    #assign first digit to a variable
+                    pos = text.index(char)
+                    #use variable to ensure that no digit between alphabets and first digit is not a string
+                    if text[pos:].isdigit() and char != '0':
+                        return True
+                    else:
+                        return False
+        
 
 
 if __name__ == "__main__":
