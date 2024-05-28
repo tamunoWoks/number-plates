c618d064875d614099ae991151cfcc7e803b8dff
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-28 12:45:45 -0600

    In test.py, check to ensure that input starts with at least 2 alphabets

--- test_plates.py
+++ test_plates.py
@@ -12,3 +12,7 @@ def test_alnum():
     assert is_valid('CS50') == True
     assert is_valid('ABC350') == True
     assert is_valid('ABC,45') == False
+
+def test_start():
+    assert is_valid('123GAB') == False
+    assert is_valid('ABC350') == True
