8ac8a68dc65a980775d266e40bd8aabb054676b8
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-28 12:53:44 -0600

    Cleaned up code

--- plates.py
+++ plates.py
@@ -5,21 +5,22 @@ def main():
     else:
         print("Invalid")
 
+
 def is_valid(text):
-    #check if plate is within 2-6 characters
-    if len(text) >= 2 and len(text) <=6:
-        #make sure it can accept only alphabets
+    # check if plate is within 2-6 characters
+    if len(text) >= 2 and len(text) <= 6:
+        # make sure it can accept only alphabets
         if text.isalpha():
             return True
-        #check if first two characters are alphabets and if input is alphanumeric
+        # check if first two characters are alphabets and if input is alphanumeric
         elif text.isalnum() and text[0:2].isalpha():
             for char in text:
-                #check for first digit
+                # check for first digit
                 if char.isdigit():
-                    #assign first digit to a variable
+                    # assign first digit to a variable
                     pos = text.index(char)
-                    #use variable to ensure that no digit between alphabets and first digit is not a string
-                    if text[pos:].isdigit() and char != '0':
+                    # use variable to ensure that no digit between alphabets and first digit is not a string
+                    if text[pos:].isdigit() and char != "0":
                         return True
                     else:
                         return False
@@ -27,7 +28,6 @@ def is_valid(text):
             return False
     else:
         return False
-        
 
 
 if __name__ == "__main__":
--- test_plates.py
+++ test_plates.py
@@ -1,26 +1,37 @@
 from plates import is_valid
 
+
 def test_range():
-    assert is_valid('AB') == True
-    assert is_valid('ABC350') == True
-    assert is_valid('ABC12345') == False
+    assert is_valid("AB") == True
+    assert is_valid("ABC350") == True
+    assert is_valid("ABC12345") == False
+
 
 def test_all_alpha():
-    assert is_valid('ABCDEF') == True
+    assert is_valid("ABCDEF") == True
+
 
 def test_alnum():
-    assert is_valid('CS50') == True
-    assert is_valid('ABC350') == True
-    assert is_valid('ABC,45') == False
+    assert is_valid("CS50") == True
+    assert is_valid("ABC350") == True
+    assert is_valid("ABC,45") == False
+
 
 def test_start():
-    assert is_valid('123GAB') == False
-    assert is_valid('ABC350') == True
+    assert is_valid("123GAB") == False
+    assert is_valid("ABC350") == True
+
 
 def test_first_digit():
-    assert is_valid('ABH023') == False
-    assert is_valid('ABC350') == True
+    assert is_valid("ABH023") == False
+    assert is_valid("ABC350") == True
+
 
 def test_all_digit():
-    assert is_valid('350') == False
-    assert is_valid('712345') == False
+    assert is_valid("350") == False
+    assert is_valid("712345") == False
+
+
+def test_num_place():
+    assert is_valid("ABD456") == True
+    assert is_valid("AB12HU") == False
